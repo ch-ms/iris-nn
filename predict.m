daa6e3b88670bfc83c5ccea2fb5a1febe40e5858
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2016-05-30 23:11:35 +0300

    Prediction matrix

--- predict.m
+++ predict.m
@@ -7,13 +7,7 @@ function [p, pm] = predict(X, Theta1, Theta2)
   a2 = [ones(size(a2)(1), 1), a2];
 
   z3 = a2 * Theta2';
-  a3 = sigmoid(z3);
+  pm = sigmoid(z3);
 
-  [v p] = max(a3, [], 2);
-
-  pm = zeros(size(a3));
-  for i = 1:size(p)(1)
-    pm(i, p(i)) = 1;
-  endfor
-
+  [v p] = max(pm, [], 2);
 end
